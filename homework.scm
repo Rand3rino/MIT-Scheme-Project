ee6565ccb700c5e52df70be7d14e0509c2dc2e20
Rand3rino <37269917+Rand3rino@users.noreply.github.com>
2019-04-03 22:03:42 -0400

    1,2,4,6,10

---
+++ homework.scm
@@ -0,0 +1,47 @@
+;;; 1
+(define pi 3.14159)
+(define e 2.71828)
+pi
+e
+
+;;; 2
+(define (circle-specs radius) 
+  (let (
+	(pi_times_r
+	  (* pi radius) ) )
+       (list (* 2 pi_times_r)
+	     (* radius pi_times_r)
+       )
+  )
+)
+(circle-specs 10)
+
+;;; 4
+(define list1 (list 1 2 3 4))
+(define list2 (list 5 6 7 8))
+(map + list1 list2 )
+
+
+;;; 6
+(define (fib x) 
+  (if (< x 2)
+      x
+      (+ (fib (- x 1)) (fib (- x 2)))
+  )
+)
+(fib 5)
+(fib 10)
+
+;;; 8
+(define (fib-list x) 
+
+
+;;; 10
+(define (remainder a b) 
+  (if (< a b)
+      (remainder a (- b a) )
+      b
+  )
+)
+
+(remainder 4 13)
