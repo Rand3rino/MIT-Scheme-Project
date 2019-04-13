7f0f255b2f9525403491c35267b7f56a4f6aa0ab
Sam Ventocilla <36198345+ventocis@users.noreply.github.com>
2019-04-13 01:12:58 -0400

    Update homework.scm

--- homework.scm
+++ homework.scm
@@ -1,3 +1,6 @@
+
+
+
 ;;; 1
 (define pi 3.14159)
 (define e 2.71828)
@@ -16,11 +19,31 @@ e
 )
 (circle-specs 10)
 
+;;3
+(define (logn n val)
+    (/ (log val) (log n))
+)
+
+(logn 4 2)
+(define (log2 val)
+    (logn 2 val)
+)
+
+(log2 6)
+
 ;;; 4
 (define list1 (list 1 2 3 4))
 (define list2 (list 5 6 7 8))
 (map + list1 list2 )
 (map * list1 list2 )
+
+;;;5
+(define (dot-product vector1 vector2)
+  (reduce-right + 0 (map * vector1 vector2))
+)
+
+(dot-product (list 1 2 3 4) (list 5 6 7 8))
+
 
 ;;; 6
 (define (fib x) 
@@ -32,9 +55,36 @@ e
 (fib 5)
 (fib 10)
 
+;;7
+(define (appendList list1 start end)
+    (define start2 (+ start 1))
+    (define list2 (list start2))
+    (cond  ((< start end) (appendList (append list1 list2) start2 end))             
+	(else list1))
+)
+(define (create-list start end)
+    (define list1 (list start))
+    (appendList list1 start end)
+)
+
+(create-list 1 10)
 ;;; 8
 (define (fib-list x) 
 	(map fib (create-list 1 x)))
+
+;;9
+(define (iterate list2 num finalNum)
+     (cond ((= num finalNum) (car list2))
+     (else (iterate (cdr list2) (+ num 1) finalNum)))
+)
+
+(iterate (list 1 2 3 4) 0 2)
+
+(define (nth list1 num)
+  (iterate list1 0 num)
+)
+
+(nth (list 1 2 3 4 5 6) 3)
 
 
 ;;; 10
